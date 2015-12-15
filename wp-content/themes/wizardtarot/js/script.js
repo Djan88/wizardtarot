a1bfbb8df50190e5bb4cf1529e20ec99f9234b8c
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-15 15:42:12 +0300

    main page fix-56

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -1,43 +1,43 @@
 jQuery(function() {
     var tarot_cards = {
-            0: '/images/gallery/15-Major-Devil-min.png)',
-            1: '/images/gallery/00-Major-Fool-min.png)',
-            2: '/images/gallery/76-Minor-Wands-Queen-min.png)',
-            3: '/images/gallery/62-Minor-Cups-Queen-min.png)',
-            4: '/images/gallery/48-Minor-Swords-Queen-min.png)',
-            5: '/images/gallery/34-Minor-Discs-Queen-min.png)',
-            6: '/images/gallery/77-Minor-Wands-King-min.png)',
-            7: '/images/gallery/63-Minor-Cups-King-min.png)',
-            8: '/images/gallery/49-Minor-Swords-King-min.png)',
-            9: '/images/gallery/35-Minor-Discs-King-min.png)',
-            10: '/images/gallery/75-Minor-Wands-Knight-min.png)',
-            11: '/images/gallery/61-Minor-Cups-Knight-min.png)',
-            12: '/images/gallery/47-Minor-Swords-Knight-min.png)',
-            13: '/images/gallery/33-Minor-Discs-Knight-min.png)',
-            14: '/images/gallery/74-Minor-Wands-Page-min.png)',
-            15: '/images/gallery/60-Minor-Cups-Page-min.png)',
-            16: '/images/gallery/46-Minor-Swords-Page-min.png)',
-            17: '/images/gallery/32-Minor-Discs-Page-min.png)',
-            18: '/images/gallery/21-Major-World-min.png)',
-            19: '/images/gallery/20-Major-Judgement-min.png)',
-            20: '/images/gallery/19-Major-Sun-min.png)',
-            21: '/images/gallery/18-Major-Moon-min.png)',
-            22: '/images/gallery/17-Major-Star-min.png)',
-            23: '/images/gallery/16-Major-Tower-min.png)',
-            24: '/images/gallery/14-Major-Temperance-min.png)',
-            25: '/images/gallery/13-Major-Death-min.png)',
-            26: '/images/gallery/12-Major-Hanged-min.png)',
-            27: '/images/gallery/11-Major-Strenght-min.png)',
-            28: '/images/gallery/10-Major-Fortune-min.png)',
-            29: '/images/gallery/09-Major-Hermit-min.png)',
-            30: '/images/gallery/08-Major-Justice-min.png)',
-            31: '/images/gallery/07-Major-Chariot-min.png)',
-            32: '/images/gallery/06-Major-Lovers-min.png)',
-            33: '/images/gallery/05-Major-Hierophant-min.png)',
-            34: '/images/gallery/04-Major-Emperor-min.png)',
-            35: '/images/gallery/03-Major-Empress-min.png)',
-            36: '/images/gallery/02-Major-Priestess-min.png)',
-            37: '/images/gallery/01-Major-Magician-min.png)',
+            0: '/images/gallery/15-Major-Devil',
+            1: '/images/gallery/00-Major-Fool',
+            2: '/images/gallery/76-Minor-Wands-Queen',
+            3: '/images/gallery/62-Minor-Cups-Queen',
+            4: '/images/gallery/48-Minor-Swords-Queen',
+            5: '/images/gallery/34-Minor-Discs-Queen',
+            6: '/images/gallery/77-Minor-Wands-King',
+            7: '/images/gallery/63-Minor-Cups-King',
+            8: '/images/gallery/49-Minor-Swords-King',
+            9: '/images/gallery/35-Minor-Discs-King',
+            10: '/images/gallery/75-Minor-Wands-Knight',
+            11: '/images/gallery/61-Minor-Cups-Knight',
+            12: '/images/gallery/47-Minor-Swords-Knight',
+            13: '/images/gallery/33-Minor-Discs-Knight',
+            14: '/images/gallery/74-Minor-Wands-Page',
+            15: '/images/gallery/60-Minor-Cups-Page',
+            16: '/images/gallery/46-Minor-Swords-Page',
+            17: '/images/gallery/32-Minor-Discs-Page',
+            18: '/images/gallery/21-Major-World',
+            19: '/images/gallery/20-Major-Judgement',
+            20: '/images/gallery/19-Major-Sun',
+            21: '/images/gallery/18-Major-Moon',
+            22: '/images/gallery/17-Major-Star',
+            23: '/images/gallery/16-Major-Tower',
+            24: '/images/gallery/14-Major-Temperance',
+            25: '/images/gallery/13-Major-Death',
+            26: '/images/gallery/12-Major-Hanged',
+            27: '/images/gallery/11-Major-Strenght',
+            28: '/images/gallery/10-Major-Fortune',
+            29: '/images/gallery/09-Major-Hermit',
+            30: '/images/gallery/08-Major-Justice',
+            31: '/images/gallery/07-Major-Chariot',
+            32: '/images/gallery/06-Major-Lovers',
+            33: '/images/gallery/05-Major-Hierophant',
+            34: '/images/gallery/04-Major-Emperor',
+            35: '/images/gallery/03-Major-Empress',
+            36: '/images/gallery/02-Major-Priestess',
+            37: '/images/gallery/01-Major-Magician',
         },
         tarot_cards_count = 1,
         tarot_cur_card = 0,
@@ -52,7 +52,8 @@ jQuery(function() {
             } else {
                 // Вывод первой карты
                 if(tarot_devil_status == 0){
-                    tarot_cell.find('.hexagon-in2').removeClass('tarot_empty_cell').css('backgroundImage', tarot_themplate_url+tarot_cards[tarot_cur_card]);
+                    tarot_cell.find('.hexagon-in2').removeClass('tarot_empty_cell').css('backgroundImage', tarot_themplate_url+tarot_cards[tarot_cur_card]+'-min.png)');
+                    tarot_cell.find('.overlay').find('a').attr('href', tarot_themplate_url+tarot_cards[tarot_cur_card]+'.png)');
                     delete tarot_cards[tarot_cur_card];
                     tarot_devil_status = 1;
                     tarot_cards_count ++;
@@ -60,7 +61,8 @@ jQuery(function() {
                 } else {
                     tarot_cur_card = (Math.round(Math.random() * (37 - 0)));
                     if(tarot_cards[tarot_cur_card]){
-                        tarot_cell.find('.hexagon-in2').removeClass('tarot_empty_cell').addClass('tarot_full_cell').css('backgroundImage', tarot_themplate_url+tarot_cards[tarot_cur_card]);
+                        tarot_cell.find('.hexagon-in2').removeClass('tarot_empty_cell').addClass('tarot_full_cell').css('backgroundImage', tarot_themplate_url+tarot_cards[tarot_cur_card]+'-min.png)');
+                        tarot_cell.find('.overlay').find('a').attr('href', tarot_themplate_url+tarot_cards[tarot_cur_card]+'.png)');
                         delete tarot_cards[tarot_cur_card];
                         console.log('right—'+tarot_cur_card);
                         console.log(tarot_cards);
