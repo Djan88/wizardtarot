21fe1e8188d5f29c07d913264ec93215e04667f5
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-16 18:00:51 +0300

    tarot fix-1

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -85,6 +85,7 @@ jQuery(function() {
                 jQuery(this).removeClass('tarot_card_reject');
                 jQuery(this).find('.hexagon-in2').removeClass('tarot_full_cell');
                 tarot_devil_cell.find('.hexagon-in2').removeClass('tarot_devil_cell');
+                console.log(tarot_cards_count);
                 if (tarot_cards_count == 3) {
                     jQuery('.tarot_to_photo').removeClass('hidden');
                 };
