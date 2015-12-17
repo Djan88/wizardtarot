d0f37465d58903877ccc54509091c50b83e73600
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-17 12:52:12 +0300

    tarot dev-13

--- wp-content/themes/wizardtarot/functions.php
+++ wp-content/themes/wizardtarot/functions.php
@@ -1,4 +1,14 @@
 <?php
+
+function users_redirect(){
+wp_redirect(site_url('/'));
+die();
+}
+if(!current_user_can('manage_options')){
+add_action('admin_init','users_redirect');
+add_filter('login_redirect', 'users_redirect');
+}
+
 function uploadImageFile() { // Note: GD library is required for this function
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
