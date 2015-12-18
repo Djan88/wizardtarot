26c9142d5d217e73d8b8d66dd9c609036d71e05d
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-12-18 11:29:58 +0300

    tarot dev-34

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -152,19 +152,19 @@ jQuery(function() {
                     d12Val+= 9;
                     jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
                     jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
-                    jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
+                    jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/wizardtarot/images/daemon.png) 0 0/100% no-repeat');
                 } else if (count_animation >= 292 && count_animation <= 344){
                     cur_animation_val += 1.5;
                     d12Val+= 9;
                     jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
                     jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
-                    jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
+                    jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/wizardtarot/images/daemon.png) 0 0/100% no-repeat');
                 } else {
                     d12Val+= 9;
                     cur_animation_val += 1.5;
                     jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
                     jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
-                    jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
+                    jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/wizardtarot/images/daemon.png) 0 0/100% no-repeat');
                 }
             } else {
                 clearInterval(phaseOne);
