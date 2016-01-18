84a39d709ecbab54b259394ef40348f018aa48f1
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-01-18 11:19:41 +0300

    new prot-23

--- wp-content/themes/wizardtarot/frontpage.php
+++ wp-content/themes/wizardtarot/frontpage.php
@@ -87,24 +87,24 @@
                             <table class="table table-bordered">
                               <tbody>
                                 <tr>
-                                  <td><img src="<?php bloginfo('template_url'); ?>/images/air_tarot.png" alt=""></td>
-                                  <td>Описание блока Воздух</td>
-                                  <td><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="air">Воздух</button></td>
+                                  <td class="elemnts_td_img"><img src="<?php bloginfo('template_url'); ?>/images/air_tarot.png" alt=""></td>
+                                  <td class="elemnts_td_note">Описание блока Воздух</td>
+                                  <td class="elemnts_td_btn"><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="air">Воздух</button></td>
                                 </tr>
                                 <tr>
-                                  <td><img src="<?php bloginfo('template_url'); ?>/images/earth_tarot.png" alt=""></td>
-                                  <td>Описание блока Земля</td>
-                                  <td><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="ground">Земля</button></td>
+                                  <td class="elemnts_td_img"><img src="<?php bloginfo('template_url'); ?>/images/earth_tarot.png" alt=""></td>
+                                  <td class="elemnts_td_note">Описание блока Земля</td>
+                                  <td class="elemnts_td_btn"><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="ground">Земля</button></td>
                                 </tr>
                                 <tr>
-                                  <td><img src="<?php bloginfo('template_url'); ?>/images/fire_tarot.png" alt=""></td>
-                                  <td>Описание блока Огонь</td>
-                                  <td><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="fire">Огонь</button></td>
+                                  <td class="elemnts_td_img"><img src="<?php bloginfo('template_url'); ?>/images/fire_tarot.png" alt=""></td>
+                                  <td class="elemnts_td_note">Описание блока Огонь</td>
+                                  <td class="elemnts_td_btn"><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="fire">Огонь</button></td>
                                 </tr>
                                 <tr>
-                                  <td><img src="<?php bloginfo('template_url'); ?>/images/water_tarot.png" alt=""></td>
-                                  <td>Описание блока Вода</td>
-                                  <td><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="water">Вода</button></td>
+                                  <td class="elemnts_td_img"><img src="<?php bloginfo('template_url'); ?>/images/water_tarot.png" alt=""></td>
+                                  <td class="elemnts_td_note">Описание блока Вода</td>
+                                  <td class="elemnts_td_btn"><button type="button" class="btn btn-default btn-lg elem-choice-item" data-elem="water">Вода</button></td>
                                 </tr>
                               </tbody>
                             </table>
