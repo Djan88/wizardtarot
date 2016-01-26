06f5cd2707db35a8db4618fef6a3f89d9529c0fa
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-01-26 13:24:26 +0300

    new prot-73

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -228,6 +228,7 @@ jQuery(function() {
                 jQuery('#draggable1').css({
                     background: 'url(/wp-content/themes/wizardtarot/images/oct_2.png) 0 0/100% no-repeat',
                     color: '#000',
+                    transform: 'scale(2)',
                     paddingTop: '10px'
                 });
                 jQuery('#draggable0').css({
@@ -241,6 +242,7 @@ jQuery(function() {
                     color: 'crimson',
                     borderColor: 'crimson',
                     zIndex: '1',
+                    transform: 'scale(1)',
                     paddingTop: '8px'
                 });
                 jQuery('#draggable1').text('D+');
