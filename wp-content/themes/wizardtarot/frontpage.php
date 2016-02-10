c657095951685add94311d99d47b68cb3da079aa
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-02-10 11:33:05 +0300

    elem fix-3

--- wp-content/themes/wizardtarot/frontpage.php
+++ wp-content/themes/wizardtarot/frontpage.php
@@ -131,7 +131,7 @@
                            </p>
                           </div>
                           <div class="bs-example elems_table_block">
-                            <div class="row">
+                            <div>
                               <div class="col-md-3 elem_wrap">
                                 <div class="elem_circle">
                                   <div class="oval_wrap">
