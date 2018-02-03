da6266d78af1a69d45a1d22b526628e43de5482c
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-02-03 16:41:06 +0300

    elems-17

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -243,7 +243,7 @@ if (backStatus == 'true') {
     }
     var devil_w = parseInt(jQuery('.elems_devil-client').css('width'));
     console.log(devil_w);
-    jQuery('.elems_devil-client').css('height', );
+    jQuery('.elems_devil-client').css('height', (devil_w * 1,32)+'px');
   }
   //На шаг назад
   jQuery('.tarot_back').on('click', function(event) {
