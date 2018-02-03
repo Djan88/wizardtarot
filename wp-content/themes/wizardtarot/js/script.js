ea89442852e7814dc1acb7976ef1440f73988941
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-02-03 17:25:10 +0300

    elems-26

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -2378,7 +2378,8 @@ jQuery(function() {
        size: 110
     });
 });
-jQuery( "#devil_move" ).draggable({ containment: "#elems_devil-devil", scroll: false });
+jQuery( "#devil_move" ).draggable();
+
 jQuery(window).resize(function(event) {
   devil_w = parseInt(jQuery('.elems_devil-client').css('width'));
   jQuery('.elems_devil-client, .elems_devil-devil').css('height', (devil_w * 1.5)+'px');
