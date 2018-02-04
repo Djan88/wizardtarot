37b40ab90dc8aeb1eae6827c86a9ccf1f68f2154
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-02-04 18:33:06 +0300

    elems-42

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -2389,16 +2389,15 @@ jQuery(function() {
         knife = jQuery('.devil_move').css('top');
         knife = knife.substr(0, knife.length - 2);
         knifeDate = new Date();
-        console.log('test');
-        // knifeDateDiff = knifeDate - knifeDateOld;
-        // knife_rate_class = 'knife_rate-'+knife;
-        // knife_rate_class_dotted = '.knife_rate-'+knife;
-        // jQuery('.elems_graph').append('<div class='+knife_rate_class+'></div>');
-        // jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
-        //     top: +knife+45+'px',
-        //     width: knifeDateDiff*2+'px'
-        // });
-        // knifeDateOld = knifeDate;
+        knifeDateDiff = knifeDate - knifeDateOld;
+        knife_rate_class = 'knife_rate-'+knife;
+        knife_rate_class_dotted = '.knife_rate-'+knife;
+        jQuery('.elems_graph').append('<div class='+knife_rate_class+'></div>');
+        jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
+            top: +knife+45+'px',
+            width: knifeDateDiff*2+'px'
+        });
+        knifeDateOld = knifeDate;
       }
     });
 });
