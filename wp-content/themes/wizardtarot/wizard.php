41bfd200dd4e2eaa50f3352b28226c9abf4b6e58
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-02-05 15:44:07 +0300

    elems-75

--- wp-content/themes/wizardtarot/wizard.php
+++ wp-content/themes/wizardtarot/wizard.php
@@ -118,6 +118,7 @@
                         </div>
                         <div class="col-xs-1">
                           <button type="button" class="btn btn-default btn-lg elem_prot_start btn-default pull-right">Старт</button>
+                          <div class="chart" data-percent="0"><span>0</span>%</div>
                         </div>
                       </div>
                       <div class="col-xs-5">
