e8a2a08281f06d88c0a04b459edcc93fb730ecea
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-02-12 23:10:17 +0300

    elem prot-38

--- wp-content/themes/wizardtarot/js/script.js
+++ wp-content/themes/wizardtarot/js/script.js
@@ -695,7 +695,7 @@ if (backStatus == 'true') {
           // e_one.css('marginTop', parseInt(jQuery('#draggable31').css('top'))/2+'px');
           jQuery('.first_cards')
             .removeClass('hidden')
-            .css('marginTop', parseInt(e_one.css('top')) - (parseInt(jQuery('.first_cards').css('height'))/2) +'px');
+            .css('marginTop', parseInt(e_one.css('top')) - (parseInt(jQuery('.first_cards').css('height'))/1.5) +'px');
           jQuery('.elem_card_1').css('background', e_dama);
           jQuery('.elem_card_2').css('background', e_king);
           jQuery('.elem_card_3').css('background', e_paje);
