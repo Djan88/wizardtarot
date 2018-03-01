3b381ca1d8217b8452af855f1e1293ae7781b59b
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-01 19:47:44 +0300

    tarot fix-6

--- wp-content/themes/wizardtarot/wizard.php
+++ wp-content/themes/wizardtarot/wizard.php
@@ -75,7 +75,13 @@
                     <div class="container tarot_screen">
                       <div class="col-xs-2"></div>
                       <div class="col-xs-3 tarot_scene tarot_prot_returned"></div>
-                      <div class="col-xs-2"></div>
+                      <div class="col-xs-2">
+                        <div class="progress progress-striped active">
+                          <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+                            <span class="sr-only">45% Complete</span>
+                          </div>
+                        </div>
+                      </div>
                       <div class="col-xs-3 tarot_scene tarot_prot_card"></div>
                       <div class="col-xs-2"></div>
                     </div>
